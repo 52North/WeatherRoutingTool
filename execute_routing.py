24e03f860b8c71031d978769fa9713721eaa822d
Katharina Demmich <k.demmich@52north.org>
2023-07-21 16:59:40 +0200

    rework code style execute_routing.py

--- execute_routing.py
+++ execute_routing.py
@@ -1,31 +1,20 @@
-import io
+import datetime as dt
 import logging
-import os
 import warnings
-import sys
 import logging.handlers
-from logging import FileHandler, Formatter
-
-# # Added because of package import error
-# # Define current working directory
-# os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-#
-# # Add current working directory as a search location for Python modules and Packages
-# sys.path.append(os.path.join(os.getcwd(), ""))
-
-
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 import WeatherRoutingTool.config as config
 import WeatherRoutingTool.utils.graphics as graphics
-from WeatherRoutingTool.ship.ship import *
-from WeatherRoutingTool.weather import *
+from WeatherRoutingTool.ship.ship import Tanker
+from WeatherRoutingTool.weather import WeatherCondFromFile, WeatherCondODC
 from WeatherRoutingTool.constraints.constraints import *
 from WeatherRoutingTool.algorithms.routingalg_factory import *
 from WeatherRoutingTool.utils.maps import Map
 
+
 def merge_figures_to_gif(path, nof_figures):
     graphics.merge_figs(path, nof_figures)
+
 
 if __name__ == "__main__":
     ##
@@ -64,7 +53,7 @@ if __name__ == "__main__":
     start = (r_la1, r_lo1)
     finish = (r_la2, r_lo2)
     start_time = dt.datetime.strptime(config.START_TIME, '%Y%m%d%H')
-    map = Map(lat1,lon1,lat2,lon2)
+    map = Map(lat1, lon1, lat2, lon2)
 
     # *******************************************
     # initialise boat
@@ -79,59 +68,67 @@ if __name__ == "__main__":
     # *******************************************
     # initialise weather
     wt = WeatherCondFromFile(model, start_time, hours, 3)
-    #wt = WeatherCondODC(model, start_time,hours,3)
+    # wt = WeatherCondODC(model, start_time,hours,3)
     wt.set_map_size(map)
     wt.read_dataset(windfile)
-    #wt.write_data('/home/kdemmich/MariData/Code/Data/WheatherFiles')
+    # wt.write_data('/home/kdemmich/MariData/Code/Data/WheatherFiles')
 
     # *******************************************
     # initialise constraints
     pars = ConstraintPars()
     land_crossing = LandCrossing()
-    water_depth = WaterDepth(config.DEPTH_DATA, config.BOAT_DROUGHT, map)
-    #seamarks_crossing = SeamarkCrossing()
-    #water_depth.write_reduced_depth_data('/home/kdemmich/MariData/Code/Data/DepthFiles/ETOPO_renamed.nc')
+    water_depth = WaterDepth(config.DEPTH_DATA, config.BOAT_DROUGHT, map,
+                             False)
+    # seamarks_crossing = SeamarkCrossing()
+    # water_depth.write_reduced_depth_data(
+    # '/home/kdemmich/MariData/Code/Data/DepthFiles/ETOPO_renamed.nc')
     # water_depth.plot_depth_map_from_file(depthfile, lat1, lon1, lat2, lon2)
     on_map = StayOnMap()
     on_map.set_map(lat1, lon1, lat2, lon2)
     continuous_checks_seamarks = SeamarkCrossing()
     continuous_checks_land = LandPolygonsCrossing()
 
-    #Simulationsstudie 2, Thames <-> Gothenburg
-    #over_waypoint1 = PositiveConstraintPoint(51.128497, 1.700607)
-    #over_waypoint2 = PositiveConstraintPoint(51.753670, 2.600120)
-    #over_waypoint3 = PositiveConstraintPoint(53.121505, 2.722398)
+    # Simulationsstudie 2, Thames <-> Gothenburg
+    # over_waypoint1 = PositiveConstraintPoint(51.128497, 1.700607)
+    # over_waypoint2 = PositiveConstraintPoint(51.753670, 2.600120)
+    # over_waypoint3 = PositiveConstraintPoint(53.121505, 2.722398)
 
-    #over_waypoint4 = PositiveConstraintPoint(55.796111, 3.100278)  # good weather
-    #over_waypoint4 = PositiveConstraintPoint(54.608889, 6.179722)  # ok weather
-    #over_waypoint4 = PositiveConstraintPoint(55.048333, 5.130000)  # bad weather
+    # over_waypoint4 = PositiveConstraintPoint(55.796111, 3.100278)  # good
+    # weather
+    # over_waypoint4 = PositiveConstraintPoint(54.608889, 6.179722)  # ok
+    # weather
+    # over_waypoint4 = PositiveConstraintPoint(55.048333, 5.130000)  # bad
+    # weather
 
-    #Simulationsstudie 2, Thames <-> Bordeaux
-    #over_waypoint1 = PositiveConstraintPoint(51.098903, 1.549883)
-    #over_waypoint2 = PositiveConstraintPoint(50.600152, 0.609062)
-    #over_waypoint3 = PositiveConstraintPoint(49.988757, -2.915933)
+    # Simulationsstudie 2, Thames <-> Bordeaux
+    # over_waypoint1 = PositiveConstraintPoint(51.098903, 1.549883)
+    # over_waypoint2 = PositiveConstraintPoint(50.600152, 0.609062)
+    # over_waypoint3 = PositiveConstraintPoint(49.988757, -2.915933)
 
     over_waypoint3 = PositiveConstraintPoint(49.988757, -2.915933)
-    #over_waypoint4 = PositiveConstraintPoint(48.850777, -5.870688)
-    
-    #over_waypoint4 = PositiveConstraintPoint(45.715, -5.502222)      # good weather
-    #over_waypoint4 = PositiveConstraintPoint(54.608889, 6.179722)   # ok weather
-    #over_waypoint4 = PositiveConstraintPoint(55.048333, 5.130000)   # bad weather
+    # over_waypoint4 = PositiveConstraintPoint(48.850777, -5.870688)
 
+    # over_waypoint4 = PositiveConstraintPoint(45.715, -5.502222)      #
+    # good weather
+    # over_waypoint4 = PositiveConstraintPoint(54.608889, 6.179722)   # ok
+    # weather
+    # over_waypoint4 = PositiveConstraintPoint(55.048333, 5.130000)   # bad
+    # weather
 
     constraint_list = ConstraintsList(pars)
     constraint_list.add_neg_constraint(land_crossing)
     constraint_list.add_neg_constraint(on_map)
     constraint_list.add_neg_constraint(water_depth)
-    constraint_list.add_neg_constraint(continuous_checks_seamarks, 'continuous')
-    constraint_list.add_neg_constraint(continuous_checks_land, 'continuous')
+    # constraint_list.add_neg_constraint(continuous_checks_seamarks,
+    # 'continuous')
+    # constraint_list.add_neg_constraint(continuous_checks_land, 'continuous')
 
-    #constraint_list.add_pos_constraint(over_waypoint1)
-    #constraint_list.add_pos_constraint(over_waypoint2)
-    #constraint_list.add_pos_constraint(over_waypoint3)
+    # constraint_list.add_pos_constraint(over_waypoint1)
+    # constraint_list.add_pos_constraint(over_waypoint2)
+    # constraint_list.add_pos_constraint(over_waypoint3)
     constraint_list.add_pos_constraint(over_waypoint3)
 
-    #constraint_list.add_pos_constraint(over_waypoint4)
+    # constraint_list.add_pos_constraint(over_waypoint4)
     constraint_list.print_settings()
 
     # *******************************************
@@ -144,11 +141,14 @@ if __name__ == "__main__":
     # routing
     min_fuel_route = min_fuel_route.execute_routing(boat, wt, constraint_list)
     min_fuel_route.print_route()
-    #min_fuel_route.write_to_file(str(min_fuel_route.route_type) + "route.json")
-    min_fuel_route.return_route_to_API(routepath + '/' + str(min_fuel_route.route_type) + ".json")
+    # min_fuel_route.write_to_file(str(min_fuel_route.route_type) +
+    # "route.json")
+    min_fuel_route.return_route_to_API(
+        routepath + '/' + str(min_fuel_route.route_type) + ".json")
 
     # *******************************************
     # plot route in constraints
     fig, ax = plt.subplots(figsize=(12, 7))
-    water_depth.plot_route_in_constraint(min_fuel_route, graphics.get_colour(1), fig, ax)
+    water_depth.plot_route_in_constraint(min_fuel_route,
+                                         graphics.get_colour(1), fig, ax)
     plt.savefig(figurepath + '/route_waterdepth.png')
