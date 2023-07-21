53aa129faa225830787105079050f88e2a54168d
Katharina Demmich <k.demmich@52north.org>
2023-07-21 16:12:48 +0200

    add environment variables to github workflow and implement usage of a separate .env.test file for unit tests

--- load_wrt.sh
+++ load_wrt.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
 pwd=${PWD}
+run_modus=$1
+
+if [ "${run_modus}" == 'test' ]
+then
+  WRT_PATH=${pwd}'/WeatherRoutingTool'
+else
+  if ! [ -n "${WRT_PATH}" ]
+  then
+    echo 'Please set the path to the base folder of the WeatherRoutingTool package'
+  fi
+fi
+
+echo 'WRT_PATH is set to ' ${WRT_PATH}
 
 cd ${WRT_PATH}
-echo 'moving to path ' ${PWD}
-export $(grep -v '^#' .env | xargs)
+
+if [ "${run_modus}" == 'test' ]
+then
+  echo 'Setting environment variables for test case'
+  export $(grep -v '^#' .env.test | xargs)
+else
+  echo 'Setting environment variables for normal use case'
+  export $(grep -v '^#' .env | xargs)
+fi
+
 cd ${pwd}
-echo 'back in path ' ${PWD}
+
+
