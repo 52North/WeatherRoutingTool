387ed5a5f1d92538d5131fb4e5fd974079062f7f
Katharina Demmich <k.demmich@52north.org>
2023-07-24 11:44:24 +0200

    rework utils regarding code style

--- WeatherRoutingTool/utils/maps.py
+++ WeatherRoutingTool/utils/maps.py
@@ -5,7 +5,7 @@ class Map():
     lon2: float
 
     def __init__(self, lat1, lon1, lat2, lon2):
-        self.lat1=lat1
-        self.lon1=lon1
-        self.lat2=lat2
-        self.lon2=lon2
+        self.lat1 = lat1
+        self.lon1 = lon1
+        self.lat2 = lat2
+        self.lon2 = lon2
--- WeatherRoutingTool/utils/unit_conversion.py
+++ WeatherRoutingTool/utils/unit_conversion.py
@@ -9,16 +9,19 @@ import WeatherRoutingTool.utils.formatting as form
 
 logger = logging.getLogger('WRT.weather')
 
+
 def mps_to_knots(vals):
     """convert the Meters/second to knots.
     knot is a unit of speed equal to one nautical mile per hour, exactly 1.852 km/h."""
     return vals * 3600.0 / 1852.0
+
 
 def knots_to_mps(vals):
     """convert the Meters/second to knots.
         knot is a unit of speed equal to one nautical mile per hour, exactly 1.852 km/h."""
 
     return vals * 1852.0 / 3600.0
+
 
 def round_time(dt=None, round_to=60):
     """
@@ -41,22 +44,30 @@ def round_time(dt=None, round_to=60):
 
     return dt + datetime.timedelta(0, rounding - seconds, - dt.microsecond)
 
+
 def degree_to_pmpi(degrees):
-    if(degrees>=360): degrees = degrees-360
-    if(degrees<=-360): degrees = degrees+360
-    if(degrees>180): degrees = degrees-360
-    if(degrees<-180): degrees = degrees+360
+    if (degrees >= 360):
+        degrees = degrees - 360
+    if (degrees <= -360):
+        degrees = degrees + 360
+    if (degrees > 180):
+        degrees = degrees - 360
+    if (degrees < -180):
+        degrees = degrees + 360
     degrees = math.radians(degrees)
     return degrees
+
 
 def convert_nptd64_to_h(time):
     time = time.astype('timedelta64[m]') / np.timedelta64(1, 'm')
     return time
 
+
 def convert_nptd64_to_ints(time):
     dt64 = np.datetime64(time)
     ts = (dt64 - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
     return ts
+
 
 def check_dataset_spacetime_consistency(ds1, ds2, coord, ds1_name, ds2_name):
     coord1 = ds1[coord].to_numpy()
@@ -72,11 +83,10 @@ def check_dataset_spacetime_consistency(ds1, ds2, coord, ds1_name, ds2_name):
         res2 = convert_nptd64_to_h(res2)
         shift2 = convert_nptd64_to_h(shift2)
 
-    logger.info(form.get_log_step('Checking consistency of datasets ' + ds1_name + ' and ' + ds2_name + ' for variable/dimension ' + coord))
-    logger.info(form.get_log_step('Resolutions are: ' + ds1_name + ':' + str(res1) + ', ' +  ds2_name + ':' + str(res2) , 2))
     logger.info(form.get_log_step(
-        ds2_name + ' is shifted wrt. ' + ds1_name + ' by ' + str(shift2), 2))
+        'Checking consistency of datasets ' + ds1_name + ' and ' + ds2_name + ' for variable/dimension ' + coord))
+    logger.info(
+        form.get_log_step('Resolutions are: ' + ds1_name + ':' + str(res1) + ', ' + ds2_name + ':' + str(res2), 2))
+    logger.info(form.get_log_step(ds2_name + ' is shifted wrt. ' + ds1_name + ' by ' + str(shift2), 2))
 
     return res1, res2, shift2
-
-
