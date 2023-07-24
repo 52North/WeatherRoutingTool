88b0faa59621ec53cb70e6d0ef90215aae36c52b
Katharina Demmich <k.demmich@52north.org>
2023-07-24 11:43:42 +0200

    rework utils/graphics regarding code style

--- WeatherRoutingTool/utils/formatting.py
+++ WeatherRoutingTool/utils/formatting.py
@@ -9,17 +9,21 @@ import numpy
 def print_line():
     print('---------------------------------------------------')
 
+
 def print_step(stepnote, istep=0):
-    step = "   " * (istep+1) + stepnote
+    step = "   " * (istep + 1) + stepnote
     print(step)
 
+
 def get_log_step(stepnote, istep=0):
-    step = "   " * (istep+1) + stepnote
+    step = "    " * (istep + 1) + stepnote
     return step
 
-def print_current_time(function : str, start_time : time.time):
-    time_passed=time.time()-start_time
+
+def print_current_time(function: str, start_time: time.time):
+    time_passed = time.time() - start_time
     print('Time after ' + function + ':' + str(time_passed))
+
 
 class NumpyArrayEncoder(JSONEncoder):
     def default(self, obj):
--- WeatherRoutingTool/utils/graphics.py
+++ WeatherRoutingTool/utils/graphics.py
@@ -1,6 +1,3 @@
-"""Module to create map with route."""
-"""Cartopy is a Python package designed for geospatial data processing in order to produce maps 
-and other geospatial data analyses"""
 import cartopy.crs as ccrs
 import cartopy.feature as cf
 import matplotlib.pyplot as plt
@@ -9,11 +6,6 @@ import numpy as np
 from geovectorslib import geod
 from matplotlib.figure import Figure
 from PIL import Image
-
-"""lat1 : initial latitude 
-   lat2 : Final latitude
-   lon1 : initial longitude 
-   lon2 : Final Longitude """
 
 
 def get_gcr_points(lat1, lon1, lat2, lon2, n_points=10):
@@ -35,18 +27,14 @@ def get_gcr_points(lat1, lon1, lat2, lon2, n_points=10):
 
 def create_maps(lat1, lon1, lat2, lon2, dpi, winds, n_maps):
     """Return map figure."""
-    fig = Figure(
-        figsize=(1600 / dpi, 800 * n_maps / dpi),
-        dpi=dpi)
-    fig.set_constrained_layout_pads(
-        w_pad=4. / dpi,
-        h_pad=4. / dpi)
+    fig = Figure(figsize=(1600 / dpi, 800 * n_maps / dpi), dpi=dpi)
+    fig.set_constrained_layout_pads(w_pad=4. / dpi, h_pad=4. / dpi)
 
     """Add gcrs between provided points to the map figure."""
     path = get_gcr_points(lat1, lon1, lat2, lon2, n_points=10)
     print(path)
     for i in range(n_maps):
-        ax = fig.add_subplot(n_maps+1, 1, i+1, projection=ccrs.PlateCarree())
+        ax = fig.add_subplot(n_maps + 1, 1, i + 1, projection=ccrs.PlateCarree())
         ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
         ax.add_feature(cf.LAND)
         ax.add_feature(cf.OCEAN)
@@ -55,9 +43,7 @@ def create_maps(lat1, lon1, lat2, lon2, dpi, winds, n_maps):
 
         hour = i // 3 * 3
         u, v, lats, lons = winds[int(hour)]
-        ax.barbs(lons, lats, u, v, length=5,
-                 sizes=dict(emptybarb=0.25, spacing=0.2, height=0.5),
-                 linewidth=0.95)
+        ax.barbs(lons, lats, u, v, length=5, sizes=dict(emptybarb=0.25, spacing=0.2, height=0.5), linewidth=0.95)
 
         lats = [x[0] for x in path]
         lons = [x[1] for x in path]
@@ -68,21 +54,11 @@ def create_maps(lat1, lon1, lat2, lon2, dpi, winds, n_maps):
 
 def create_map(lat1, lon1, lat2, lon2, dpi):
     """Return map figure."""
-    fig = Figure(
-        figsize=(1200 / dpi, 420 / dpi),
-        dpi=dpi)
-    fig.set_constrained_layout_pads(
-        w_pad=4. / dpi,
-        h_pad=4. / dpi)
+    fig = Figure(figsize=(1200 / dpi, 420 / dpi), dpi=dpi)
+    fig.set_constrained_layout_pads(w_pad=4. / dpi, h_pad=4. / dpi)
     ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
-    fig.subplots_adjust(
-        left=0.05,
-        right=0.95,
-        bottom=0,
-        top=1,
-        wspace=0,
-        hspace=0)
-    #ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
+    fig.subplots_adjust(left=0.05, right=0.95, bottom=0, top=1, wspace=0, hspace=0)
+    # ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
     ax.add_feature(cf.LAND)
     ax.add_feature(cf.OCEAN)
     ax.add_feature(cf.COASTLINE)
@@ -97,14 +73,14 @@ def plot_barbs(fig, winds):
     lats = winds['lats_u']
     lons = winds['lons_u']
 
-    #rebinx=5  #CMEMS
-    #rebiny=11
+    # rebinx=5  #CMEMS
+    # rebiny=11
 
-    #rebinx=1   #NCEP
-    #rebiny=1
+    # rebinx=1   #NCEP
+    # rebiny=1
 
-    rebinx= 10   #depth
-    rebiny= 10
+    rebinx = 10  # depth
+    rebiny = 10
 
     u = rebin(u, rebinx, rebiny)
     v = rebin(v, rebinx, rebiny)
@@ -112,9 +88,7 @@ def plot_barbs(fig, winds):
     lons = rebin(lons, rebinx, rebiny)
 
     ax = fig.get_axes()[0]
-    ax.barbs(lons, lats, u, v, length=5,
-             sizes=dict(emptybarb=0.25, spacing=0.2, height=0.5),
-             linewidth=0.95)
+    ax.barbs(lons, lats, u, v, length=5, sizes=dict(emptybarb=0.25, spacing=0.2, height=0.5), linewidth=0.95)
     # ax.quiver(lons, lats, u, v)
     return fig
 
@@ -128,6 +102,7 @@ def plot_gcr(fig, lat1, lon1, lat2, lon2):
     ax = fig.get_axes()[0]
     ax.plot(lons, lats, 'g')
     return fig
+
 
 '''
 def plot_route(fig, route: RouteParams, colour):
@@ -149,75 +124,73 @@ def plot_route(fig, route: RouteParams, colour):
     return fig
     '''
 
+
 def plot_legend(fig):
     ax = fig.get_axes()[0]
     ax.legend()
     return fig
 
+
 def get_colour(i):
     colours = ['darkred', 'gold', 'seagreen', 'peachpuff', 'darkviolet']
-    if(i>4):
+    if (i > 4):
         raise ValueError('currently only 5 colours available, asking for' + str(i))
     return colours[i]
 
-def rebin(a,rebinx, rebiny):
-    modx=a.shape[0] % rebinx
-    mody=a.shape[1] % rebiny
 
+def rebin(a, rebinx, rebiny):
+    modx = a.shape[0] % rebinx
+    mody = a.shape[1] % rebiny
 
     if not (modx == 0):
-        for imod in range(0,modx):
-            a=np.delete(a,0,0)
+        for imod in range(0, modx):
+            a = np.delete(a, 0, 0)
 
     if not (mody == 0):
-       for imod in range(0, mody):
+        for imod in range(0, mody):
             a = np.delete(a, 0, 1)
 
-    newshape_x = int(a.shape[0]/rebinx)
-    newshape_y = int(a.shape[1]/rebiny)
+    newshape_x = int(a.shape[0] / rebinx)
+    newshape_y = int(a.shape[1] / rebiny)
 
     sh = newshape_x, rebinx, newshape_y, rebiny
     return a.reshape(sh).mean(-1).mean(1)
 
+
 def merge_figs(path, ncounts):
-        fig, ax = plt.subplots(figsize=(12, 8), dpi=500)
-        ax.axis('off')
-        image_list = []
-        impath = ''
+    fig, ax = plt.subplots(figsize=(12, 8), dpi=500)
+    ax.axis('off')
+    image_list = []
+    impath = ''
 
-        fig.subplots_adjust(
-            left=0.01,
-            right=0.98,
-            bottom=0,
-            top=1,
-            wspace=0,
-            hspace=0)
+    fig.subplots_adjust(left=0.01, right=0.98, bottom=0, top=1, wspace=0, hspace=0)
 
-        for iIm in range(1,ncounts+1):
-            impath = path + 'fig' + str(iIm) + 'p.png'
-            print('Reading image ', impath)
-            im_temp = Image.open(impath)
-            im_plot = plt.imshow(im_temp)
-            image_list.append([im_plot])
+    for iIm in range(1, ncounts + 1):
+        impath = path + 'fig' + str(iIm) + 'p.png'
+        print('Reading image ', impath)
+        im_temp = Image.open(impath)
+        im_plot = plt.imshow(im_temp)
+        image_list.append([im_plot])
 
-        ani = animation.ArtistAnimation(fig, image_list, interval=1000, blit=True,
-                                        repeat_delay=10)
+    ani = animation.ArtistAnimation(fig, image_list, interval=1000, blit=True, repeat_delay=10)
 
-        writergif = animation.PillowWriter(fps=1, bitrate=1000)
-        ani.save(path + str(ani) + '.gif', writer=writergif)
+    writergif = animation.PillowWriter(fps=1, bitrate=1000)
+    ani.save(path + str(ani) + '.gif', writer=writergif)
+
 
 def get_hist_values_from_boundaries(bin_boundaries, contend_unnormalised):
     centres = np.array([])
     widths = np.array([])
     contents = np.array([])
-    for i in range(0, bin_boundaries.shape[0]-1):
-        width_temp = (bin_boundaries[i+1]-bin_boundaries[i])
-        cent_temp = bin_boundaries[i] + width_temp/2
-        cont_temp = contend_unnormalised[i]/width_temp
-        centres=np.append(centres, cent_temp)
+    for i in range(0, bin_boundaries.shape[0] - 1):
+        width_temp = (bin_boundaries[i + 1] - bin_boundaries[i])
+        cent_temp = bin_boundaries[i] + width_temp / 2
+        cont_temp = contend_unnormalised[i] / width_temp
+        centres = np.append(centres, cent_temp)
         widths = np.append(widths, width_temp)
         contents = np.append(contents, cont_temp)
-    return {"bin_content" : contents, "bin_centres" : centres, "bin_widths" : widths}
+    return {"bin_content": contents, "bin_centres": centres, "bin_widths": widths}
+
 
 def get_hist_values_from_widths(bin_widths, contend_unnormalised):
     centres = np.array([])
@@ -225,12 +198,14 @@ def get_hist_values_from_widths(bin_widths, contend_unnormalised):
     cent_temp = 0
     for i in range(0, bin_widths.shape[0]):
         cont_temp = 0
-        cent_temp = cent_temp + bin_widths[i]/2
-        if(bin_widths[i]>0): cont_temp = contend_unnormalised[i]/bin_widths[i]
-        centres=np.append(centres, cent_temp)
+        cent_temp = cent_temp + bin_widths[i] / 2
+        if (bin_widths[i] > 0):
+            cont_temp = contend_unnormalised[i] / bin_widths[i]
+        centres = np.append(centres, cent_temp)
         contents = np.append(contents, cont_temp)
-        cent_temp = cent_temp + bin_widths[i]/2
-    return {"bin_content" : contents, "bin_centres" : centres}
+        cent_temp = cent_temp + bin_widths[i] / 2
+    return {"bin_content": contents, "bin_centres": centres}
+
 
 def get_accumulated_dist(dist_arr):
     dist_acc = np.array([])
@@ -238,9 +213,7 @@ def get_accumulated_dist(dist_arr):
 
     for dist in dist_arr:
         dist_acc_temp = dist + full_dist
-        dist_acc=np.append(dist_acc, dist_acc_temp)
+        dist_acc = np.append(dist_acc, dist_acc_temp)
         full_dist = full_dist + dist
 
     return dist_acc
-
-
