8ccd0f5f83e799877bbd7a5cf209759d05cb142a
Martin Pontius <m.pontius@52north.org>
2023-11-30 10:40:02 +0100

    [WRT] Update packaging

--- setup.py
+++ setup.py
@@ -1,46 +1,4 @@
-import os
+import setuptools
 
-from setuptools import setup, find_packages
-
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-setup(
-    name='WeatherRoutingTool',
-    version='0.1',
-    include_package_data=True,
-    packages=find_packages(),
-    url='',
-    license='',
-    author='52N Developers',
-    author_email='info@52north.org',
-    description='Python package for weather routing',
-    install_requires=[
-      'bbox',
-      'dask',
-      'datacube',
-      'geographiclib',
-      'geopandas',
-      'geovectorslib',
-      'global_land_mask',
-      'lxml',
-      'matplotlib',
-      'numpy == 1.23.4',
-      'pandas',
-      'pymoo',
-      'pytest',
-      'Pillow',
-      'scipy>=1.10.0',
-      'setuptools',
-      'shapely',
-      'sqlalchemy<2.0,>=1.4',
-      'xarray',
-      'netcdf4'
-    ]
-)
-os.system('pip install wheel')
-os.system('pip install git+https://github.com/52North/MariGeoRoute#subdirectory=data/maridatadownloader')
-os.system('pip install cartopy')
+if __name__ == "__main__":
+    setuptools.setup()
