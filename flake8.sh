6f93cbc9067a7e72225f832f2c186f578d17c9dd
Martin Pontius <m.pontius@52north.org>
2023-12-15 18:10:55 +0100

    [WRT] Add tests folder to linting script

--- flake8.sh
+++ flake8.sh
@@ -1,3 +1,3 @@
 flake8 --max-line-length=120 \
        --exclude WeatherRoutingTool/additional_code \
-       --extend-ignore F401,F403,F405,E711 ./WeatherRoutingTool ./execute_routing.py ./compare_routes.py
+       --extend-ignore F401,F403,F405,E711 ./WeatherRoutingTool ./execute_routing.py ./compare_routes.py ./tests
