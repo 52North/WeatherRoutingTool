8d4c66ea70d79f5404211d4d28c177de3d440a26
Martin Pontius <m.pontius@52north.org>
2024-01-11 13:23:41 +0100

    Update excludes in flake8.sh

--- flake8.sh
+++ flake8.sh
@@ -1,4 +1,3 @@
 flake8 --max-line-length=120 \
-       --exclude WeatherRoutingTool/additional_code \
        --extend-ignore F401,F403,F405,E711 ./WeatherRoutingTool ./tests ./execute_routing.py ./compare_routes.py \
        ./write_specific_route.py
