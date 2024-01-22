7478d31d698a2902effe379094d8a0911176acb8
Martin Pontius <m.pontius@52north.org>
2024-01-22 18:05:57 +0100

    Fix flake8 script

--- flake8.sh
+++ flake8.sh
@@ -1,2 +1,2 @@
 flake8 --max-line-length=120 \
-       --extend-ignore F401,F403,F405,E711 ./WeatherRoutingTool ./tests ./execute_routing.py ./compare_routes.py
+       --extend-ignore F401,F403,F405,E711 ./WeatherRoutingTool ./tests ./cli.py ./compare_routes.py
