20fa853675fe44cd5174f6d44f2f363aa27ad4b9
Martin Pontius <m.pontius@52north.org>
2024-01-25 14:43:38 +0100

    Some more clean-ups in test_maripower

--- WeatherRoutingTool/test_maripower_settings_for_different_courses.py
+++ WeatherRoutingTool/test_maripower_settings_for_different_courses.py
@@ -1,7 +1,7 @@
 import argparse
-import copy
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from datetime import datetime, timedelta
 
 import WeatherRoutingTool.utils.graphics as graphics
@@ -12,10 +12,9 @@ from WeatherRoutingTool.ship.ship import Tanker
 from WeatherRoutingTool.weather_factory import WeatherFactory
 
 
-def run_maripower_test_scenario(calmfactor, windfactor, wavefactor, waypoint_dict, maripower_scenario,
-                                weather_scenario, draught):
-    boat = Tanker(config)
-    boat.Draugh = draught
+def run_maripower_test_scenario(config_obj, calmfactor, windfactor, wavefactor, waypoint_dict, maripower_scenario,
+                                weather_scenario):
+    boat = Tanker(config_obj)
     boat.set_ship_property('WindForcesFactor', windfactor)
     boat.set_ship_property('WaveForcesFactor', wavefactor)
     boat.set_ship_property('CalmWaterFactor', calmfactor)
@@ -54,7 +53,7 @@ def plot_power_vs_courses(nominator_list, label_list, denominator_obj, courses, 
 
     ax.legend()
 
-    plt.savefig(figuredir + name + '.png')
+    plt.savefig(os.path.join(figuredir, name + '.png'))
 
     plt.cla()
     plt.close()
@@ -83,7 +82,7 @@ def plot_polar_power(curve_list, label_list, courses, figuredir, name, fuel_type
     axes.set_title(ylabel, va='bottom')
     plt.legend(bbox_to_anchor=(0.5, -0.27), loc='lower center')
     # plt.tight_layout()
-    plt.savefig(figuredir + name + '_polar.png')
+    plt.savefig(os.path.join(figuredir, name + '_polar.png'))
 
 
 if __name__ == "__main__":
@@ -97,19 +96,19 @@ if __name__ == "__main__":
 
     routename = 'original_resistances_calm_weather'
     windfile = config.WEATHER_DATA
-    depthfile = config.DEPTH_DATA
-    coursesfile = config.COURSES_FILE
     figurepath = get_figure_path()
     time_resolution = config.DELTA_TIME_FORECAST
     time_forecast = config.TIME_FORECAST
     departure_time = datetime.strptime(config.DEPARTURE_TIME, '%Y-%m-%dT%H:%MZ')
     lat1, lon1, lat2, lon2 = config.DEFAULT_MAP
     default_map = Map(lat1, lon1, lat2, lon2)
+    lat_start, lon_start, lat_end, lon_end = config.DEFAULT_ROUTE  # lat_end and lon_end are not used
+    coord_res = 0.1
 
-    weather_type = 'rough_weather'  # rought_weather, calm_weather
+    weather_type = 'rough_weather'  # rough_weather, calm_weather
     wind_speed = -99
     VHMO = -99
-    draught = 9.5
+    VTPK = -99
 
     if weather_type == 'rough_weather':
         wind_speed = 12.5
@@ -121,7 +120,7 @@ if __name__ == "__main__":
         VTPK = 1.5
 
     if wind_speed == -99 or VHMO == -99:
-        raise ValueError('windspeed or VHM0 not set!')
+        raise ValueError('wind_speed or VHM0 not set!')
 
     u_comp = 0
     v_comp = - wind_speed
@@ -135,7 +134,8 @@ if __name__ == "__main__":
         'v-component_of_wind_height_above_ground': v_comp,
         'VHM0': VHMO,
         'VMDR': 0,
-        'VTPK': VTPK}
+        'VTPK': VTPK
+    }
 
     maripower_test_scenarios_calm = {'original': 1., '95perc_calm': 0.95, '105perc_calm': 1.05, '80perc_wind': 1.,
                                      '120perc_wind': 1., '80perc_wave': 1., '120perc_wave': 1.}
@@ -146,37 +146,29 @@ if __name__ == "__main__":
 
     wt = WeatherFactory.get_weather(data_mode=config.DATA_MODE, file_path=windfile, departure_time=departure_time,
                                     time_forecast=time_forecast, time_resolution=time_resolution,
-                                    default_map=default_map, var_dict=var_dict)
+                                    default_map=default_map, var_dict=var_dict, coord_res=coord_res)
     # fig, ax = plt.subplots(figsize=(12, 7))
     # wt.plot_weather_map(fig, ax, "2023-08-16T12:00:00", "wind")
 
-    courses = np.linspace(0, 360, 37)       # scan courses from 0 to 360° in steps of 10°
-    courses_plot = copy.deepcopy(courses)
-    start_lats = np.repeat(49.05, courses.shape)         # dummy times, distances
-    start_lons = np.repeat(30.89, courses.shape)
+    courses = np.linspace(0, 360, 37)  # scan courses from 0 to 360° in steps of 10°
+    start_lats = np.repeat(lat_start, courses.shape)
+    start_lons = np.repeat(lon_start, courses.shape)
     time = np.repeat(datetime.now(), courses.shape)
-    dist = np.repeat(0, courses.shape)
     travel_times = np.linspace(0, 36, 37)
     for i in range(0, time.shape[0]):
         time[i] = time[i] + timedelta(minutes=travel_times[i])
-
-    route_start = (49.05, 30.89)
-    route_end = route_start
 
     waypoint_dict = {
         'courses': courses,
         'start_lats': start_lats,
         'start_lons': start_lons,
-        'time': time,
-        'dist': dist,
-        'travel_times': travel_times,
-        'route_start': route_start,
-        'route_end': route_end
+        'time': time
     }
 
     shipparams_vec = {}
     for key in maripower_test_scenarios_wind:
         shipparams_vec[key] = run_maripower_test_scenario(
+            config,
             maripower_test_scenarios_calm[key],
             maripower_test_scenarios_wind[key],
             maripower_test_scenarios_wave[key],
@@ -188,29 +180,29 @@ if __name__ == "__main__":
     nominator_list = [shipparams_vec['95perc_calm'], shipparams_vec['105perc_calm']]
     label_list = ['95% Glattwasserwiderstand', '105% Glattwasserwiderstand']
     plot_power_vs_courses(nominator_list, label_list, shipparams_vec['original'], courses, figurepath,
-                          'calmwaterres_' + weather_type, 'power', draught)
+                          'calmwaterres_' + weather_type, 'power')
 
     nominator_list = [shipparams_vec['80perc_wind'], shipparams_vec['120perc_wind']]
     label_list = ['80% Zusatzwiderstand Wind', '120% Zusatzwiderstand Wind']
     plot_power_vs_courses(nominator_list, label_list, shipparams_vec['original'], courses, figurepath,
-                          'windres_' + weather_type, 'power', draught)
+                          'windres_' + weather_type, 'power')
 
     nominator_list = [shipparams_vec['80perc_wave'], shipparams_vec['120perc_wave']]
     label_list = ['80% Zusatzwiderstand Seegang', '120% Zusatzwiderstand Seegang']
     plot_power_vs_courses(nominator_list, label_list, shipparams_vec['original'], courses, figurepath,
-                          'waveres_' + weather_type, 'power', draught)
+                          'waveres_' + weather_type, 'power')
 
     curve_list = [shipparams_vec['original'], shipparams_vec['95perc_calm'], shipparams_vec['105perc_calm']]
     label_list = ['original', '95% Glattwasserwiderstand', '105% Glattwasserwiderstand']
     plot_polar_power(curve_list, label_list, courses, figurepath,
-                     'calmwaterres_' + weather_type, 'power', draught)
+                     'calmwaterres_' + weather_type, 'power')
 
     curve_list = [shipparams_vec['original'], shipparams_vec['80perc_wind'], shipparams_vec['120perc_wind']]
     label_list = ['original', '80% Zusatzwiderstand Wind', '120% Zusatzwiderstand Wind']
     plot_polar_power(curve_list, label_list, courses, figurepath,
-                     'windres_' + weather_type, 'power', draught)
+                     'windres_' + weather_type, 'power')
 
     curve_list = [shipparams_vec['original'], shipparams_vec['80perc_wave'], shipparams_vec['120perc_wave']]
     label_list = ['original', '80% Zusatzwiderstand Seegang', '120% Zusatzwiderstand Seegang']
     plot_polar_power(curve_list, label_list, courses, figurepath,
-                     'waveres_' + weather_type, 'power', draught)
+                     'waveres_' + weather_type, 'power')
