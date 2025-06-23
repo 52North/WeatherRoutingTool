2f46f928f55c4015e0b7a242b6295031a05b1694
Katharina Demmich <k.demmich@52north.org>
2025-06-23 12:16:48 +0200

    change reading of histogram options to flags & improved handling of arguments

--- compare_routes.py
+++ compare_routes.py
@@ -76,53 +76,69 @@ def plot_power_vs_dist_ratios(rp_list, rp_str_list, scenario_str, power_type='fu
 if __name__ == "__main__":
     # Compare variations of resistances for specific routes
 
+    hist_dict = {
+        'weather': False,
+        'route': False,
+        'power_vs_dist': False,
+        'fuel_vs_dist': False,
+        'acc_fuel_vs_dist': False,
+        'power_vs_lon': False,
+        'fuel_vs_lon': False,
+        'power_vs_lat': False,
+        'fuel_vs_lat': False,
+        'power_vs_dist_showing_weather': False,
+        'power_vs_dist_ratios': False,
+        'fuel_vs_dist_ratios': False
+    }
+
     parser = argparse.ArgumentParser(description='Weather Routing Tool')
-    parser.add_argument('--base-dir', help="Base directory of route geojson files (absolute path)",
+    required_args = parser.add_argument_group('required arguments')
+    optional_args = parser.add_argument_group('optional arguments')
+    required_args.add_argument('--base-dir', help="Base directory of route geojson files (absolute path).",
                         required=True, type=str)
-    parser.add_argument('--figure-dir', help="Figure directory (absolute path)",
+    required_args.add_argument('--figure-dir', help="Figure directory (absolute path).",
                         required=True, type=str)
-    parser.add_argument('--file-list', nargs="*", required=True, type=str)
-    parser.add_argument('--name-list', nargs="*", required=True, type=str)
-    parser.add_argument('--scenario-str', required=False, default=' ', type=str)
-    parser.add_argument('--wind-file', required=False, default=' ', type=str)
+    required_args.add_argument('--file-list', help="List of input route files (absolute paths).", nargs="*", required=True, type=str)
+    required_args.add_argument('--name-list', help="List of legend entries for individual routes. Same ordering as for flag --file-list.", nargs="*", required=True, type=str)
+    required_args.add_argument('--hist-list', help="List of histograms that shall be plotted. The following types are supported: " + str(hist_dict.keys()), nargs="*", required=True, type=str)
+    optional_args.add_argument('--scenario-str', help="String that can be added to the final plot for further description.", required=False, default=' ', type=str)
+    optional_args.add_argument('--wind-file', help="Absolute path to weather data.", required=False, default=' ', type=str)
 
+    # read arguments
     args = parser.parse_args()
-
     figurefile = args.figure_dir
-
     filelist = args.file_list
     rp_str_list = args.name_list
+    hist_list = args.hist_list
 
     rp_list=[]
     for path in filelist:
         rp_list.append(RouteParams.from_file(path))
 
-    if len(rp_list) != len(rp_str_list):
-        raise ValueError('Every histogram needs a name for the legend.')
-    if len(rp_list) < 2:
-        raise ValueError('You need to pass at least two histograms.')
-
     scenario_str = args.scenario_str
-
     windfile = args.wind_file
     depth_data = ""
     set_up_logging()
 
-    do_plot_weather = False
-    do_plot_route = False
-    do_plot_power_vs_dist = True
-    do_plot_fuel_vs_dist = True
-    do_plot_acc_fuel_vs_dist = False
+    # catch faulty configuration
+    found_hist = False
+    for option in hist_list:
+        for hist in hist_dict:
+            if option == hist:
+                hist_dict[option] = True
+                found_hist = True
+        if not found_hist:
+            parser.print_help()
+            raise ValueError('The option "' + option + '" is not available for plotting!')
+        found_hist = False
 
-    do_plot_power_vs_lon = False
-    do_plot_fuel_vs_lon = False
-    do_plot_power_vs_lat = False
-    do_plot_fuel_vs_lat = False
+    if len(rp_list) != len(rp_str_list):
+        parser.print_help()
+        raise ValueError('Every histogram needs a name for the legend.')
 
-    do_plot_power_vs_dist_showing_weather = False
-    do_plot_power_vs_dist_ratios = True
-    do_plot_fuel_vs_dist_ratios = True
-    do_write_fuel = True
+    if (len(rp_list) < 2) and (hist_dict['power_vs_dist_ratios'] or hist_dict['fuel_vs_dist_ratios']):
+        parser.print_help()
+        raise ValueError('You need to pass at least two histograms for ratio plots.')
 
     ##
     # init weather
@@ -138,7 +154,7 @@ if __name__ == "__main__":
     plot_time = dt.datetime.strptime(time_for_plotting, '%Y-%m-%dT%H:%MZ')
     default_map = Map(lat1, lon1, lat2, lon2)
 
-    if do_plot_weather:
+    if hist_dict['weather']:
         wt = WeatherFactory.get_weather("from_file", windfile, departure_time_dt, time_forecast, 3, default_map)
 
         fig, ax = plt.subplots(figsize=(12, 7))
@@ -154,7 +170,7 @@ if __name__ == "__main__":
 
     ##
     # plotting routes in depth profile
-    if do_plot_route:
+    if hist_dict['route']:
         fig, ax = plt.subplots(figsize=graphics.get_standard('fig_size'))
         ax.axis('off')
         ax.xaxis.set_tick_params(labelsize='large')
@@ -168,35 +184,35 @@ if __name__ == "__main__":
 
     ##
     # plotting  vs. distance
-    if do_plot_power_vs_dist:
+    if hist_dict['power_vs_dist']:
         plot_power_vs_dist(rp_list, rp_str_list, scenario_str, 'power')
 
-    if do_plot_fuel_vs_dist:
+    if hist_dict['fuel_vs_dist']:
         plot_power_vs_dist(rp_list, rp_str_list, scenario_str, 'fuel')
 
     ##
     # plotting  accumulated vs. distance
 
-    if do_plot_acc_fuel_vs_dist:
+    if hist_dict['acc_fuel_vs_dist']:
         plot_acc_power_vs_dist(rp_list, rp_str_list, 'fuel')
 
     ##
     # plotting power vs. coordinate
-    if do_plot_power_vs_lat:
+    if hist_dict['power_vs_lat']:
         plot_power_vs_coord(rp_list, rp_str_list, 'lat', 'power')
 
-    if do_plot_fuel_vs_lat:
+    if hist_dict['fuel_vs_lat']:
         plot_power_vs_coord(rp_list, rp_str_list, 'lat', 'fuel')
 
-    if do_plot_power_vs_lon:
+    if hist_dict['power_vs_lon']:
         plot_power_vs_coord(rp_list, rp_str_list, 'lon', 'power')
 
-    if do_plot_fuel_vs_lon:
+    if hist_dict['fuel_vs_lon']:
         plot_power_vs_coord(rp_list, rp_str_list, 'lon', 'fuel')
 
     ##
     # plotting power vs dist vs weather
-    if do_plot_power_vs_dist_showing_weather:
+    if hist_dict["power_vs_dist_showing_weather"]:
         fig, ax = plt.subplots(figsize=(12, 8), dpi=96)
         for irp in range(0, len(rp_list)):
             rp_list[irp].plot_power_vs_dist_with_weather(rp_list, rp_str_list, len(rp_list))
@@ -204,23 +220,22 @@ if __name__ == "__main__":
 
     ##
     # plotting power vs dist ratios
-    if do_plot_power_vs_dist_ratios:
+    if hist_dict["power_vs_dist_ratios"]:
         plot_power_vs_dist_ratios(rp_list, rp_str_list, scenario_str, 'power')
 
-    if do_plot_fuel_vs_dist_ratios:
+    if hist_dict["fuel_vs_dist_ratios"]:
         plot_power_vs_dist_ratios(rp_list, rp_str_list, scenario_str, 'fuel')
 
     ##
-    # write full fuel
-    if do_write_fuel:
-        print('Full fuel consumption:')
-        for irp in range(0, len(rp_list)):
-            print(rp_str_list[irp] + ': ' + str(rp_list[irp].get_full_fuel()))
+    # write fuel consumption, travel distance and time
+    print('Full fuel consumption:')
+    for irp in range(0, len(rp_list)):
+        print(rp_str_list[irp] + ': ' + str(rp_list[irp].get_full_fuel()))
 
-        print('Full travel dist:')
-        for irp in range(0, len(rp_list)):
-            print(rp_str_list[irp] + ': ' + str(rp_list[irp].get_full_dist()))
+    print('Full travel dist:')
+    for irp in range(0, len(rp_list)):
+        print(rp_str_list[irp] + ': ' + str(rp_list[irp].get_full_dist()))
 
-        print('Full travel time:')
-        for irp in range(0, len(rp_list)):
-            print(rp_str_list[irp] + ': ' + str(rp_list[irp].get_full_travel_time()))
+    print('Full travel time:')
+    for irp in range(0, len(rp_list)):
+        print(rp_str_list[irp] + ': ' + str(rp_list[irp].get_full_travel_time()))
