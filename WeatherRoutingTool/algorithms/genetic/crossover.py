989d8377d21bdf6b366132b3d44c5d4e0e76f1ed
Katharina Demmich <k.demmich@52north.org>
2025-10-23 14:10:37 +0200

    bug fix: evaluate offspring and not parents in OffspringRejectionBase

--- WeatherRoutingTool/algorithms/genetic/crossover.py
+++ WeatherRoutingTool/algorithms/genetic/crossover.py
@@ -92,8 +92,8 @@ class OffspringRejectionCrossover(CrossoverBase):
             o1, o2 = self.crossover(p1.copy(), p2.copy())
 
             if (
-                    utils.get_constraints(p1, self.constraints_list) or
-                    utils.get_constraints(p2, self.constraints_list)
+                    utils.get_constraints(o1, self.constraints_list) or
+                    utils.get_constraints(o2, self.constraints_list)
             ):
                 Y[0, k, 0] = p1
                 Y[1, k, 0] = p2
