bug fix: correct calculation of full fuel consumption in genetic problem definition

WeatherRoutingTool/algorithms/genetic/problem.py
@@ -52,5 +52,5 @@ class RoutingProblem(ElementwiseProblem):
             route_dict['start_times'], )
 
         fuel = shipparams.get_fuel_rate()
-        fuel = (fuel / 3600) * route_dict['travel_times']
+        fuel = fuel * route_dict['travel_times']
         return np.sum(fuel), shipparams