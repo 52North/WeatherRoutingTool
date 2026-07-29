c728cebe18ff72770c10bc9860d625a24482d67c
kabarmaz <k.baranyilmaz11@gmail.com>
2026-07-29 12:08:59 +0200

    [tidy] removed old rng function

--- WeatherRoutingTool/algorithms/genetic/utils.py
+++ WeatherRoutingTool/algorithms/genetic/utils.py
@@ -19,19 +19,6 @@ logger = logging.getLogger("WRT.genetic")
 RNG = np.random.default_rng()
 
 
-def set_random_generator(seed: int | None = None) -> np.random.Generator:
-    """Random generator for reproducibility of results
-
-    :param seed: Seed for the random generator. If None, a random seed is used.
-    :type seed: int | None
-    :return: Random generator
-    :rtype: np.random.Generator"""
-
-    global RNG
-    RNG = np.random.default_rng(seed)
-    return RNG
-
-
 def get_random_generator() -> np.random.Generator:
     return RNG
 
